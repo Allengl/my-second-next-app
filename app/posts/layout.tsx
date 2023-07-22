950bbbe2ccc34e5c90f1baeea3d94a70b2dc6bda
Allengl <guolong613@gmail.com>
2023-07-22 13:31:04 +0800

    feat: create title

--- app/posts/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const layout: React.FC = () => {
-  return (
-    <div> Navigate to top</div>
-  )
-}
-
-export default layout
--- app/posts/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page: React.FC = () => {
-  return (
-    <div>POSTS </div>
-  )
-}
-
-export default page
